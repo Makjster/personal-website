072e08f7f679260c90447b910d843d77711401d5
Matti Gerd Johannsen <Matti.Johannsen@gmx.de>
2021-01-11 15:59:55 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,48 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" href="images/favicon.png" sizes="16x16">
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/main.css" type="text/css"/>
+    <title>Matti Johannsen</title>
+</head>
+
+<body>
+<img src="images/matti_johannsen.jpg" class="img-fluid center" alt="Picture of Matti Johannsen">
+
+<div class="container content">
+    <div class="title">
+        <h1>Matti Johannsen</h1>
+
+</div>
+    <div class="row">
+        <div class="col"
+    <p><h3>About me:<h3><br>
+        My name is Matti Johannsen and I am studying Computer Science at the RWTH University Aachen.<br>
+        During this time I work as a student assistant in the area of software development at the chair of <a href="https://www.ip.rwth-aachen.de/" target="_blank">Individualized Production</a>.<br>
+        From time to time I also do some freelance web design. Here you can see a <a href="https://annegretbleisteiner.de/" target="_blank">Reference</a>.<br>
+    </p>
+    </div>
+    <div class="col px-md-5">
+        <h3>Technologies I worked with:</h3><br>
+        <p>
+            Good knowledge: C#, Python, SQL <br>
+            Decent knowledge: PHP, CSS, HTML, JavaScript<br>
+            Exposed to: Java, C++, C <br>
+            Other skills: Software Development, Database design, Analytical thinking, determination, Networks
+        </p>
+    </div>
+    <div class="row">
+        <p style="text-align: center">
+            <a href="mailto:Matti.Johannsen@gmx.de">Email</a> <a href="https://www.linkedin.com/in/matti-johannsen-9950871b3/" target="_blank">Linkedin</a> <a href="https://github.com/Makjster" target="_blank">GitHub</a>
+        </p>
+    </div>
+</div>
+
+<div class="footer"><a href="impressum.php">Impressum</div>
+</body>
+</html>
